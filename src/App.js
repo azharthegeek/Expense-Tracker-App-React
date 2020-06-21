b75b90e8d369f49e2aeead549fe32b14135bd087
MUHAMMAD AZHAR <azharthegeek@gmail.com>
2020-06-21 14:10:55 +0500

    Intializing Hello Word of React Expance Tracker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import './Header.js'
 import { Header } from './Header.js';
 
 function App() {
   return (
     <div className ='App-header' >
-          <h1>Hello World</h1>
+           <Header />
           <div className='container'>
-          <Header />
+            Start
           </div> 
         
           
