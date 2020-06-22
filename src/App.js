f0cb1896a1fef608e3c548d6b340ffbccf0a9549
MUHAMMAD AZHAR <azharthegeek@gmail.com>
2020-06-22 14:56:36 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Header } from './Header.js';
+
+// Import CSS
+import './App.css';
+
+// Import Components
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
+import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
+
+// Import Provider
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className ='App-header' >
-           <Header />
-    </div>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <AccountSummary />
+          <TransactionHistory />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export const Header = () => {
+    return (
+        <div className='App-header'>
+            <p>
+            <a href = "https://github.com/AZHARTHEGEEK" >
+            Expense Tracker by Muhammad Azhar
+            
+            </a>
+            </p>
+           
+            
+        </div>
+    )
+}
